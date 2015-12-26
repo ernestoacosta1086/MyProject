b82d72315418f80456628ba140acae1f51cdf9c3
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2015-12-26 14:01:43 -0500

    Recycler in the recycler activity

--- KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/RecyclerActivity.java
+++ KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/RecyclerActivity.java
@@ -4,15 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class RecyclerActivity extends AppCompatActivity {
+    @Bind(R.id.reciclador)
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +35,24 @@ public class RecyclerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Inicializar Animes
+        List<MiClaseParaLosDatos> items = new ArrayList<>();
+        items.add(new MiClaseParaLosDatos(R.drawable.angel, "Angel Beats", 230));
+        items.add(new MiClaseParaLosDatos(R.drawable.death, "Death Note", 456));
+        items.add(new MiClaseParaLosDatos(R.drawable.fate, "Fate Stay Night", 342));
+        items.add(new MiClaseParaLosDatos(R.drawable.nhk, "Welcome to the NHK", 645));
+        items.add(new MiClaseParaLosDatos(R.drawable.suzumiya, "Suzumiya Haruhi", 459));
+
+        recycler.setHasFixedSize(true);
+
+        // Usar un administrador para LinearLayout
+        RecyclerView.LayoutManager lManager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(lManager);
+
+        // Crear un nuevo adaptador
+        recycler.setAdapter(new MiAdaptador(items));
+
     }
 
 }
