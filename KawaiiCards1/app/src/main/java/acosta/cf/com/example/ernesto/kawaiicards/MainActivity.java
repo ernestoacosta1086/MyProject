6e2ad9aa7eb38f304a0abb6681e2352d16cf182d
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2015-12-26 14:37:44 -0500

    Erase main activity only recycer in the Recycler_Activity

--- KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/MainActivity.java
+++ KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/MainActivity.java
@@ -25,49 +25,16 @@ public class MainActivity extends AppCompatActivity {
     Declarar instancias globales
     */
 
-    @Bind(R.id.reciclador)
-    RecyclerView recycler;
-
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
     private RecyclerView.Adapter adapter;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-        // Inicializar Animes
-        List<MiClaseParaLosDatos> items = new ArrayList<>();
-
-
-
-        items.add(new MiClaseParaLosDatos(R.drawable.angel, "Angel Beats", 230));
-        items.add(new MiClaseParaLosDatos(R.drawable.death, "Death Note", 456));
-        items.add(new MiClaseParaLosDatos(R.drawable.fate, "Fate Stay Night", 342));
-        items.add(new MiClaseParaLosDatos(R.drawable.nhk, "Welcome to the NHK", 645));
-        items.add(new MiClaseParaLosDatos(R.drawable.suzumiya, "Suzumiya Haruhi", 459));
         ButterKnife.bind(this);
-        // Obtener el Recycler
-//        recycler = (RecyclerView) findViewById(R.id.reciclador);
-        recycler.setHasFixedSize(true);
-
-        // Usar un administrador para LinearLayout
-        RecyclerView.LayoutManager lManager = new LinearLayoutManager(this);
-        recycler.setLayoutManager(lManager);
-
-        // Crear un nuevo adaptador
-        adapter = new MiAdaptador(items);
-        recycler.setAdapter(adapter);
-
-
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
