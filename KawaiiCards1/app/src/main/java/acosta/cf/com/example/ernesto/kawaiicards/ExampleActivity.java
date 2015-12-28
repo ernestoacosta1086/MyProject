ecffcd097400b28ab972faf5427e676973c1f505
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2015-12-28 00:31:01 -0500

    New activity google maps in project

--- KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/ExampleActivity.java
+++ KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/ExampleActivity.java
@@ -3,11 +3,11 @@ package acosta.cf.com.example.ernesto.kawaiicards;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GoogleMapsActivity extends AppCompatActivity {
+public class ExampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_maps);
+        setContentView(R.layout.example_activity);
     }
 }
--- KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/MainActivity.java
+++ KawaiiCards1/app/src/main/java/acosta/cf/com/example/ernesto/kawaiicards/MainActivity.java
@@ -1,7 +1,6 @@
 package acosta.cf.com.example.ernesto.kawaiicards;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -49,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.google_maps_button)
     public void toGoogleAct(){
-        Intent intent = new Intent(getBaseContext(), GoogleMapsActivity.class);
+        Intent intent = new Intent(getBaseContext(), ExampleActivity.class);
         startActivity(intent);
     }
 
